c6ffee5a2d8e34e0b5d597824ca285152c0a68e5
Rishabh <rishabhk.ip.20@nitj.ac.in>
2024-08-25 17:29:28 +0530

    Connected Metamask

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,31 +3,40 @@ import Dashboard from "./Pages/Dashboard"
 import Darkmode from "./Components/Darkmode"
 import Shooting from "./Components/ShootingStars/Shooting"
 import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
+import { useState } from "react";
 
 
 function App() {
 
+  const [wallet, setWallet] = useState({provider:null,contract:null,signer:null});
+
+  const settingWallet = async (provider, contract, signer) => {
+    //this function sets the provider and signer or EOA address of the client that has logged in
+    setWallet({ provider: provider, contract: contract, signer: signer });
+  };
+
 
   return (
     <>
-      <div className='z-10 relative h-screen w-screen dark:text-white bg-slate-50 dark:bg-black/50  transition-all duration-500'>
+      <Toaster />
+      <div className="z-10 relative h-screen w-screen dark:text-white bg-slate-50 dark:bg-black/50  transition-all duration-500">
         <div className="absolute w-screen">
           <Darkmode />
         </div>
         <div className="">
-
-        <Shooting />
+          <Shooting />
         </div>
 
         <Router>
           <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path="/Dashboard" element={<Dashboard/>} />
+            <Route path="/" element={<Login settingWallet={settingWallet} />} />
+            <Route path="/Dashboard" element={<Dashboard />} />
           </Routes>
         </Router>
       </div>
     </>
-  )
+  );
 }
 
 export default App
